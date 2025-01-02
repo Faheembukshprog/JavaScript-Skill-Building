258a40b1ef2deea011d62622868afbe4f2ff0707
Faheembukshprog <fbux12@gmail.com>
2025-01-02 22:09:48 +0500

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // Treat all JS code as a Newer version
+
+// alert(3 + 3) // we are using node.js, not browser
+
+console.log(3 
+    + 
+    3) // code readability should be high 
+
+
+console.log("Faheem")
+
+let name = "Faheem"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol = unique
+
+
+// object 
+
+comsole.log(typeof undefined); // undefined
+comsole.log(typeof null); // object
